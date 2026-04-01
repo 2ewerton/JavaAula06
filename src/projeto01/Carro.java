7397e5a6247176c28b0676a49b24ebf4f37c107b
EWERTON VINÍCIUS ALVES DA SILVA <ewerton.alves@a.ucb.br>
2026-03-31 21:45:17 -0300

    31/03 21:44 alteracoes classe carro

--- src/projeto01/Carro.java
+++ src/projeto01/Carro.java
@@ -2,6 +2,20 @@ package projeto01;
 
 public class Carro extends Veiculo {
 	
+
+	public Carro () {
+		
+	}
+	
+	public Carro (String cor, String marca, String modelo, String combustivel, boolean ligado, Integer peso, boolean movimento, String placa) {
+		
+		super(cor, marca, modelo, combustivel, ligado, peso);
+		this.movimento = movimento;
+		this.placa = placa;
+		
+		
+	}
+	
 	private String placa;
 	private boolean movimento;
 	
